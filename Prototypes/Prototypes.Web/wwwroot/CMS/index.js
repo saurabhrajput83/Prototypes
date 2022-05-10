2816ebd2e665cf5618065c650cfe3545691c557e
Saurabh <saurabh.rajput@hotmail.com>
2022-05-10 10:51:02 +0530

    Initial Commit

---
+++ Prototypes/Prototypes.Web/wwwroot/CMS/index.js
@@ -0,0 +1,34 @@
+﻿
+// ready event
+$(document).ready(function () {
+    $('#txtInteractionDate').datepicker();
+    $('#txtDateReceivedByOrg').datepicker();
+
+});
+
+
+// angular application module
+var appModule = angular.module('appModule', []);
+
+// angular application controller
+appModule.controller('appController', ['$scope', function ($scope) {
+
+    $scope.questions = [
+        'What is product that best matches the request?',
+        'What type of Product?',
+        'Interaction Date',
+        'Date Received by Organization',
+        'Date Received by Business',
+        'What is source of the request?',
+    ];
+
+    $scope.products = [
+        'Mortgage/Home Equity'
+    ];
+
+    $scope.productTypes = [
+        'Mortgage',
+        'Home Equity'
+    ];
+
+}]);
